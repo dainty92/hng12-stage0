c9ec221aef97fdf86e40e979782fe86e739d84ba
esther-must <esther.must92@gmail.com>
2025-01-30 16:33:53 +0100

    Fixed current date time

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.get("/", (req, res) => {
     res.json({
         email: "estheradexdainty@gmail.com",
-        current_datetime: new Date().toISOString(),
+        current_datetime: new Date().toISOString().split('.')[0] + 'Z',
         github_url: "https://github.com/dainty92/hng12-stage0.git",
     });
 });
