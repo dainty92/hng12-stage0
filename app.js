1835d5f23ac24b57b5d25e3a3cb19a0f6c94a735
esther-must <esther.must92@gmail.com>
2025-01-30 15:30:19 +0100

    Stage 0

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.json({
+        email: "estheradexdainty@gmail.com",
+        current_datetime: new Date().toISOString(),
+        github_url: "https://github.com/dainty92/hng12-stage0.git",
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
